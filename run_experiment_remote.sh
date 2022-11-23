5dd26d93a7ea2f63969f9a5d5cab38bb1b224c75
Itzik ben shabat <sitzikbs@gmail.com>
2022-11-23 14:51:02 +0200

    fixed remote experiment script

--- run_experiment_remote.sh
+++ run_experiment_remote.sh
@@ -12,7 +12,7 @@ DATASET_PATH='/data1/datasets/dfaust/'
 MODEL='pn2_4d'
 STEPS_PER_UPDATE=5
 N_FRAMES=32
-BATCH_SIZE=32
+BATCH_SIZE=16
 
 N_POINTS=1024
 N_EPOCHS=30
@@ -21,5 +21,5 @@ LOGDIR='./log/'$MODEL'_f'$N_FRAMES'_p'$N_POINTS'_shuffle_'$POINTS_SHUFFLE'/'
 SET='test'
 
 python3 train_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --steps_per_update $STEPS_PER_UPDATE --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --logdir $LOGDIR --n_epochs $N_EPOCHS --n_points $N_POINTS
-python3 test_action_pred.py --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model '000005.pt' --model_path $LOGDIR --set $SET
+python3 test_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model '000005.pt' --model_path $LOGDIR --set $SET
 python3 ./evaluation/evaluate.py --results_path $LOGDIR'results/' --dataset_path $DATASET_PATH --set $SET
