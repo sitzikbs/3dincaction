fc48c0bdb04492ba43a08a222de77b9bd7d88bd7
Itzik ben shabat <sitzikbs@gmail.com>
2022-11-24 23:16:45 +0200

    fixed bug in dimensions of pn1_d4 bn4

--- models/pointnet.py
+++ models/pointnet.py
@@ -149,7 +149,7 @@ class PointNetfeat4D(nn.Module):
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(1024)
-        self.bn4 = nn.BatchNorm1d(1)
+        self.bn4 = nn.BatchNorm1d(1024)
         self.maxpool1 = torch.nn.MaxPool3d(kernel_size=[3, 1, 1], stride=[2, 1, 1])
         self.conv_t = torch.nn.Conv2d(1, 1, [1024, k_frames], 1, padding='same')
         self.global_feat = global_feat
@@ -182,7 +182,7 @@ class PointNetfeat4D(nn.Module):
         x = torch.max(x, 2, keepdim=True)[0].squeeze()
 
         x = x.reshape(b, 1, t, 1024).permute(0, 1, 3, 2)
-        x = self.bn4(self.conv_t(x)).squeeze()
+        x = self.bn4(self.conv_t(x).squeeze())
         x = self.temporal_avgpool1(x)
         x = F.interpolate(x, t, mode='linear', align_corners=True)
 
