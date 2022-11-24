6995060eef437a93863dd3d7fc9be8b73b414fc7
Itzik ben shabat <sitzikbs@gmail.com>
2022-11-24 23:03:19 +0200

    updated p1_4d to only conv and pool global features

--- models/pointnet.py
+++ models/pointnet.py
@@ -133,15 +133,23 @@ class PointNetfeat4D(nn.Module):
         self.n_frames = n_frames
         self.stn = STN3d()
         # self.conv1 = torch.nn.Conv2d(in_d, 64, [in_d, k_frames], 1, padding='same')
+        # self.conv2 = torch.nn.Conv2d(64, 128, [64, k_frames], 1, padding='same')
+        # self.conv3 = torch.nn.Conv2d(128, 1024, [128, k_frames], 1, padding='same')
+        # self.bn1 = nn.BatchNorm2d(64)
+        # self.bn2 = nn.BatchNorm2d(128)
+        # self.bn3 = nn.BatchNorm2d(1024)
+        self.temporal_maxpool1 = torch.nn.MaxPool2d(kernel_size=[1, 3], stride=[1, 2])
+        # self.temporal_maxpool2 = torch.nn.MaxPool3d(kernel_size=[1, 1, 3], stride=[1, 1, 2])
+
         self.conv1 = torch.nn.Conv1d(in_d, 64, 1)
-        self.conv2 = torch.nn.Conv2d(64, 128, [64, k_frames], 1, padding='same')
-        self.conv3 = torch.nn.Conv2d(128, 1024, [128, k_frames], 1, padding='same')
-        self.temporal_maxpool1 = torch.nn.MaxPool3d(kernel_size=[1, 1, 3], stride=[1, 1, 2])
-        self.temporal_maxpool2 = torch.nn.MaxPool3d(kernel_size=[1, 1, 3], stride=[1, 1, 2])
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+
         self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.bn3 = nn.BatchNorm2d(1024)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
         self.maxpool1 = torch.nn.MaxPool3d(kernel_size=[3, 1, 1], stride=[2, 1, 1])
+        self.conv_t = torch.nn.Conv2d(1, 1, [1024, k_frames], 1, padding='same')
         self.global_feat = global_feat
         self.feature_transform = feature_transform
         if self.feature_transform:
@@ -154,24 +162,25 @@ class PointNetfeat4D(nn.Module):
 
         x = x.reshape(b*t, k, n)
         x = F.relu(self.bn1(self.conv1(x)))  # b, 64, n, t
-        x = x.reshape(b, t, 64, n)
-        x = x.permute(0, 2, 3, 1)  # b, k, n, t
+        # x = x.reshape(b, t, 64, n)
+        # x = x.permute(0, 2, 3, 1)  # b, k, n, t
         if self.feature_transform:
-            x = x.permute(0, 3, 1, 2).reshape(-1, 64, n)  # b*t, 64, n
+            # x = x.permute(0, 3, 1, 2).reshape(-1, 64, n)  # b*t, 64, n
             trans_feat = self.fstn(x)  # b*t, 64, 64
             x = x.transpose(2, 1)  # b*t, 64, 64
             x = torch.bmm(x, trans_feat)
-            x = x.transpose(2, 1).reshape(b, t, 64, n)
+            x = x.transpose(2, 1)#.reshape(b, t, 64, n)
         else:
             trans_feat = None
 
         pointfeat = x
-        x = x.permute(0, 2, 3, 1)
+        # x = x.permute(0, 2, 3, 1)
         x = F.relu(self.bn2(self.conv2(x)))
-        x = self.temporal_maxpool1(x)
         x = self.bn3(self.conv3(x))
-        x = self.temporal_maxpool2(x)
         x = torch.max(x, 2, keepdim=True)[0].squeeze()
+        x = x.reshape(b, 1, t, 1024).permute(0, 1, 3, 2)
+        x = self.conv_t(x).squeeze()
+        x = self.temporal_maxpool1(x)
         x = F.interpolate(x, t, mode='linear', align_corners=True)
         x = x.permute(0, 2, 1)
         if self.global_feat:
