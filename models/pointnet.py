9959beb564cc9ffb38acfeb948e21ef2ca54d48a
Itzik ben shabat <sitzikbs@gmail.com>
2022-11-24 23:12:31 +0200

    updated p1_4d to batch norm after temporal conv and activate conv3

--- models/pointnet.py
+++ models/pointnet.py
@@ -149,6 +149,7 @@ class PointNetfeat4D(nn.Module):
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(1)
         self.maxpool1 = torch.nn.MaxPool3d(kernel_size=[3, 1, 1], stride=[2, 1, 1])
         self.conv_t = torch.nn.Conv2d(1, 1, [1024, k_frames], 1, padding='same')
         self.global_feat = global_feat
@@ -177,12 +178,14 @@ class PointNetfeat4D(nn.Module):
         pointfeat = x
         # x = x.permute(0, 2, 3, 1)
         x = F.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = torch.max(x, 2, keepdim=True)[0].squeeze()
+
         x = x.reshape(b, 1, t, 1024).permute(0, 1, 3, 2)
-        x = self.conv_t(x).squeeze()
+        x = self.bn4(self.conv_t(x)).squeeze()
         x = self.temporal_avgpool1(x)
         x = F.interpolate(x, t, mode='linear', align_corners=True)
+
         x = x.permute(0, 2, 1)
         if self.global_feat:
             return x, trans, trans_feat
