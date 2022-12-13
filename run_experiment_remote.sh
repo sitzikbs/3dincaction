7a6bbef0fe2bbdf2e95e6536b0b67a2677659d55
sitzikbs <sitzikbs@gmail.com>
2022-12-13 18:30:45 +0200

    updated remote experiment script

--- run_experiment_remote.sh
+++ run_experiment_remote.sh
@@ -9,7 +9,7 @@ export GOMP_CPU_AFFINITY="95-191"
 
 #DATASET_PATH='/home/sitzikbs/Datasets/dfaust/'
 DATASET_PATH='/data1/datasets/dfaust/'
-MODEL='pn1'
+MODEL='pn1_4d'
 STEPS_PER_UPDATE=1
 N_FRAMES=64
 BATCH_SIZE=16
@@ -28,6 +28,6 @@ SET='test'
 GT_JSON='gt_segments_'$GENDER'.json'
 CORREFORMER='none'
 
-#python3 train_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --steps_per_update $STEPS_PER_UPDATE --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --logdir $LOGDIR --n_epochs $N_EPOCHS --n_points $N_POINTS --sampler $SAMPLER --data_augmentation $AUGMENT --gender $GENDER --correformer $CORREFORMER
-#python3 test_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $TEST_BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model $(printf %06d $TEST_ITER).pt --model_path $LOGDIR --set $SET --gender $GENDER --correformer $CORREFORMER
+python3 train_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --steps_per_update $STEPS_PER_UPDATE --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --logdir $LOGDIR --n_epochs $N_EPOCHS --n_points $N_POINTS --sampler $SAMPLER --data_augmentation $AUGMENT --gender $GENDER --correformer $CORREFORMER
+python3 test_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $TEST_BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model $(printf %06d $TEST_ITER).pt --model_path $LOGDIR --set $SET --gender $GENDER --correformer $CORREFORMER
 python3 ./evaluation/evaluate.py --results_path $LOGDIR'results_'$(printf %06d $TEST_ITER)'/' --dataset_path $DATASET_PATH --set $SET --gt_segments_json_filename $GT_JSON --gender $GENDER
