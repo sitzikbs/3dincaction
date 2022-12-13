6109d0c5174875190135abd365af2f2a7c56cc3c
sitzikbs <sitzikbs@gmail.com>
2022-12-13 21:11:55 +0200

    updated remote execution script

--- run_experiment_remote.sh
+++ run_experiment_remote.sh
@@ -9,7 +9,7 @@ export GOMP_CPU_AFFINITY="95-191"
 
 #DATASET_PATH='/home/sitzikbs/Datasets/dfaust/'
 DATASET_PATH='/data1/datasets/dfaust/'
-MODEL='pn1_4d'
+MODEL='pn1_4d_basic'
 STEPS_PER_UPDATE=1
 N_FRAMES=64
 BATCH_SIZE=16
