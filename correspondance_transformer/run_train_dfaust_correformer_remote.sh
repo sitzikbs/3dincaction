3a2b89eebb593c0a23357b5454633cc80f9a729a
sitzikbs <sitzikbs@gmail.com>
2022-12-19 16:04:26 +0200

    udpated remote ssl training script

--- correspondance_transformer/run_train_dfaust_correformer_remote.sh
+++ correspondance_transformer/run_train_dfaust_correformer_remote.sh
@@ -1,4 +1,4 @@
-GPU_IDX=1
+GPU_IDX=3
 NUM_THREADS=96
 export OMP_NUM_THREADS=$NUM_THREADS
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
@@ -8,11 +8,12 @@ export OMP_PROC_BIND=CLOSE
 export GOMP_CPU_AFFINITY="1-96"
 
 NPOINTS=1024
-BATCH_SIZE=128
+BATCH_SIZE=256
 NHEADS=4
-DIM=256
-DFF=256
+DIM=128
+DFF=128
 DATASET_PATH='/data1/datasets/dfaust/'
 
 
-python3 train_dfaust_correformer.py --dataset_path $DATASET_PATH --dim $DIM --n_heads $NHEADS --batch_size $BATCH_SIZE --n_points $NPOINTS --d_feedforward $DFF
+#python3 train_dfaust_correformer.py --dataset_path $DATASET_PATH --dim $DIM --n_heads $NHEADS --batch_size $BATCH_SIZE --n_points $NPOINTS --d_feedforward $DFF
+python3 train_ssl_dfaust_correformer.py --dataset_path $DATASET_PATH --dim $DIM --n_heads $NHEADS --batch_size $BATCH_SIZE --n_points $NPOINTS --d_feedforward $DFF
