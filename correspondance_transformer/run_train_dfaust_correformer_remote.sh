722aa35847a89e7e5c3517d8ea66385f0c8a534c
Itzik ben shabat <sitzikbs@gmail.com>
2022-12-23 13:14:17 +0200

    updated remote script

--- correspondance_transformer/run_train_dfaust_correformer_remote.sh
+++ correspondance_transformer/run_train_dfaust_correformer_remote.sh
@@ -1,12 +1,7 @@
-GPU_IDX=0
-#NUM_THREADS=96
-#export OMP_NUM_THREADS=$NUM_THREADS
+GPU_IDX=1
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export CUDA_VISIBLE_DEVICES=$GPU_IDX
-#export OMP_SCHEDULE=STATIC
-#export OMP_PROC_BIND=CLOSE
-#export GOMP_CPU_AFFINITY="95-191"
-taskset -c 64-96
+
 
 NPOINTS=1024
 BATCH_SIZE=32
@@ -17,4 +12,4 @@ DATASET_PATH='/data1/datasets/dfaust/'
 EXP_ID='jitter0.01'
 AUG='jitter'
 
-python3 train_ssl_dfaust_correformer.py --dataset_path $DATASET_PATH --dim $DIM --n_heads $NHEADS --batch_size $BATCH_SIZE --n_points $NPOINTS --d_feedforward $DFF --exp_id $EXP_ID --aug $AUG
+taskset -c 128-160 python3 train_ssl_dfaust_correformer.py --dataset_path $DATASET_PATH --dim $DIM --n_heads $NHEADS --batch_size $BATCH_SIZE --n_points $NPOINTS --d_feedforward $DFF --exp_id $EXP_ID --aug $AUG
