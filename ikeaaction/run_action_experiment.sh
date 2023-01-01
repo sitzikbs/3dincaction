0c8c968beb83b6ff61f6fe749ee631645c68a643
Itzik ben shabat <sitzikbs@gmail.com>
2023-01-01 11:23:13 +0200

    fixed experiment execution script

--- ikeaaction/run_action_experiment.sh
+++ ikeaaction/run_action_experiment.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
 GPU_IDX=0
-NUM_THREADS=96
-export OMP_NUM_THREADS=$NUM_THREADS
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export CUDA_VISIBLE_DEVICES=$GPU_IDX
-export OMP_SCHEDULE=STATIC
-export OMP_PROC_BIND=CLOSE
-export GOMP_CPU_AFFINITY="95-191"
-#DATASET_PATH='/data1/datasets/ANU_ikea_dataset_smaller/' # on remote
-DATASET_PATH='/home/sitzikbs/datasets/ANU_ikea_dataset_smaller/' # on local
+
+DATASET_PATH='/data1/datasets/ANU_ikea_dataset_smaller/' # on remote
+#DATASET_PATH='/home/sitzikbs/datasets/ANU_ikea_dataset_smaller/' # on local
 INPUT_TYPE='pc'
 CAMERA='dev3'
 #PT_MODEL='charades' # when using images
 DB_FILENAME='ikea_annotation_db_full'
 
 #LOGDIR='/home/sitzikbs/Pycharm_projects/3dinaction/log/debug/'
-LOGDIR='./log/pn1_baseline/'
+LOGDIR='./log/pn1_4096_baseline/'
 BATCH_SIZE=10
 STEPS_PER_UPDATE=16
 FRAMES_PER_CLIP=32
@@ -28,6 +24,6 @@ POINTLET_MODE='none'  # deprecated
 N_GAUSSIANS=8         # for 3dmfv
 CORREFORMER='none'    # path or 'none'
 
-python train_i3d.py --dataset_path $DATASET_PATH --camera $CAMERA --batch_size $BATCH_SIZE --steps_per_update $STEPS_PER_UPDATE --logdir $LOGDIR --db_filename $DB_FILENAME --frames_per_clip $FRAMES_PER_CLIP --n_epochs $N_EPOCHS --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER
-python test_i3d.py --dataset_path $DATASET_PATH --device $CAMERA --model_path $LOGDIR --batch_size 3 --db_filename $DATASET_PATH$DB_FILENAME --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --model '000025.pt' --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER
-python3 ../evaluation/evaluate.py --results_path $LOGDIR'results/' --dataset_path $DATASET_PATH --mode vid
+taskset -c 128-160 python train_i3d.py --dataset_path $DATASET_PATH --camera $CAMERA --batch_size $BATCH_SIZE --steps_per_update $STEPS_PER_UPDATE --logdir $LOGDIR --db_filename $DB_FILENAME --frames_per_clip $FRAMES_PER_CLIP --n_epochs $N_EPOCHS --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER
+taskset -c 128-160 python test_i3d.py --dataset_path $DATASET_PATH --device $CAMERA --model_path $LOGDIR --batch_size 3 --db_filename $DATASET_PATH$DB_FILENAME --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --model '000025.pt' --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER
+taskset -c 128-160 python3 ../evaluation/evaluate.py --results_path $LOGDIR'results/' --dataset_path $DATASET_PATH --mode vid
--- run_experiment_remote.sh
+++ run_experiment_remote.sh
@@ -1,12 +1,8 @@
 GPU_IDX=0
-#NUM_THREADS=96
-#export OMP_NUM_THREADS=$NUM_THREADS
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export CUDA_VISIBLE_DEVICES=$GPU_IDX
-#export OMP_SCHEDULE=STATIC
-#export OMP_PROC_BIND=CLOSE
-#export GOMP_CPU_AFFINITY="95-191"
-taskset -c 64-96
+
+
 #DATASET_PATH='/home/sitzikbs/Datasets/dfaust/'
 DATASET_PATH='/data1/datasets/dfaust/'
 MODEL='pn2'
@@ -28,6 +24,6 @@ SET='test'
 GT_JSON='gt_segments_'$GENDER'.json'
 CORREFORMER='none'
 
-python3 train_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --steps_per_update $STEPS_PER_UPDATE --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --logdir $LOGDIR --n_epochs $N_EPOCHS --n_points $N_POINTS --sampler $SAMPLER --data_augmentation $AUGMENT --gender $GENDER --correformer $CORREFORMER
-python3 test_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $TEST_BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model $(printf %06d $TEST_ITER).pt --model_path $LOGDIR --set $SET --gender $GENDER --correformer $CORREFORMER
-python3 ./evaluation/evaluate.py --results_path $LOGDIR'results_'$(printf %06d $TEST_ITER)'/' --dataset_path $DATASET_PATH --set $SET --gt_segments_json_filename $GT_JSON --gender $GENDER
+taskset -c 128-160 python3 train_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --steps_per_update $STEPS_PER_UPDATE --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --logdir $LOGDIR --n_epochs $N_EPOCHS --n_points $N_POINTS --sampler $SAMPLER --data_augmentation $AUGMENT --gender $GENDER --correformer $CORREFORMER
+taskset -c 128-160 python3 test_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $TEST_BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model $(printf %06d $TEST_ITER).pt --model_path $LOGDIR --set $SET --gender $GENDER --correformer $CORREFORMER
+taskset -c 128-160 python3 ./evaluation/evaluate.py --results_path $LOGDIR'results_'$(printf %06d $TEST_ITER)'/' --dataset_path $DATASET_PATH --set $SET --gt_segments_json_filename $GT_JSON --gender $GENDER
