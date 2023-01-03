eb9b8705257117a071769cf439d791606870d7c4
sitzikbs <sitzikbs@gmail.com>
2023-01-03 13:05:21 +0200

    fixed execution script

--- ikeaaction/run_action_experiment.sh
+++ ikeaaction/run_action_experiment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GPU_IDX=2
+GPU_IDX=3
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export CUDA_VISIBLE_DEVICES=$GPU_IDX
 
@@ -26,6 +26,6 @@ N_POINTS=1024
 CORREFORMER='../correspondance_transformer/log/dfaust_N1024ff1024_d1024h8_ttypenonelr0.0001bs32reg_cat_ce/000200.pt'    # path or 'none'
 CACHE_CAPACITY=0
 
-taskset -c 128-160 python train_i3d.py --dataset_path $DATASET_PATH --camera $CAMERA --batch_size $BATCH_SIZE --steps_per_update $STEPS_PER_UPDATE --logdir $LOGDIR --db_filename $DB_FILENAME --frames_per_clip $FRAMES_PER_CLIP --n_epochs $N_EPOCHS --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER --cache_capcity $CACHE_CAPACITY
-taskset -c 128-160 python test_i3d.py --dataset_path $DATASET_PATH --device $CAMERA --model_path $LOGDIR --batch_size 3 --db_filename $DATASET_PATH$DB_FILENAME --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --model '000025.pt' --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER
-taskset -c 128-160 python3 ../evaluation/evaluate_ikeaasm.py --results_path $LOGDIR'results/' --dataset_path $DATASET_PATH --mode vid
+taskset -c 32-64 python train_i3d.py --dataset_path $DATASET_PATH --camera $CAMERA --batch_size $BATCH_SIZE --steps_per_update $STEPS_PER_UPDATE --logdir $LOGDIR --db_filename $DB_FILENAME --frames_per_clip $FRAMES_PER_CLIP --n_epochs $N_EPOCHS --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER --cache_capacity $CACHE_CAPACITY
+taskset -c 32-64 python test_i3d.py --dataset_path $DATASET_PATH --device $CAMERA --model_path $LOGDIR --batch_size 3 --db_filename $DATASET_PATH$DB_FILENAME --input_type $INPUT_TYPE --n_points $N_POINTS --pc_model $PC_MODEL --use_pointlettes $USE_POINTLETTES --pointlet_mode $POINTLET_MODE --model '000025.pt' --n_gaussians $N_GAUSSIANS --correformer $CORREFORMER
+taskset -c 32-64 python3 ../evaluation/evaluate_ikeaasm.py --results_path $LOGDIR'results/' --dataset_path $DATASET_PATH --mode vid
