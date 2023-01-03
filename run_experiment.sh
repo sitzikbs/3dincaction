8f0195cb835cd955d6d5009d2d544cb65ec6d169
Itzik ben shabat <sitzikbs@gmail.com>
2023-01-03 21:09:38 +0200

    training pn2 on dfaust

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,7 +1,7 @@
 #DATASET_PATH='/home/sitzikbs/datasets/dfaust/'
 DATASET_PATH='/home/sitzikbs/Datasets/dfaust/'
 
-MODEL='pn1_4d'
+MODEL='pn2'
 STEPS_PER_UPDATE=2
 N_FRAMES=64
 BATCH_SIZE=8
@@ -9,16 +9,17 @@ TEST_BATCH_SIZE=1
 TEST_ITER=200
 GENDER='all'
 
-N_POINTS=1024
+N_POINTS=4096
 N_EPOCHS=200
 POINTS_SHUFFLE='each_frame'
 SAMPLER='weighted'
 AUGMENT=1
 
-LOGDIR='./log/dfaust_correformer_'$GENDER'_'$MODEL'_f'$N_FRAMES'_p'$N_POINTS'_shuffle_'$POINTS_SHUFFLE'_aug'$AUGMENT'_b'$BATCH_SIZE'_u'$STEPS_PER_UPDATE'/'
+LOGDIR='./log/dfaust_'$GENDER'_'$MODEL'_f'$N_FRAMES'_p'$N_POINTS'_shuffle_'$POINTS_SHUFFLE'_aug'$AUGMENT'_b'$BATCH_SIZE'_u'$STEPS_PER_UPDATE'/'
 SET='test'
 GT_JSON='gt_segments_'$GENDER'.json'
-CORREFORMER='./correspondance_transformer/log/dfaust_N1024_ff1024d1024h8_lr0.0001bs32/000840.pt'
+#CORREFORMER='./correspondance_transformer/log/dfaust_N1024_ff1024d1024h8_lr0.0001bs32/000840.pt'
+CORREFORMER='none'
 
 python3 train_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --steps_per_update $STEPS_PER_UPDATE --frames_per_clip $N_FRAMES --batch_size $BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --logdir $LOGDIR --n_epochs $N_EPOCHS --n_points $N_POINTS --sampler $SAMPLER --data_augmentation $AUGMENT --gender $GENDER --correformer $CORREFORMER
 python3 test_action_pred.py --dataset_path $DATASET_PATH --pc_model $MODEL --frames_per_clip $N_FRAMES --batch_size $TEST_BATCH_SIZE --shuffle_points $POINTS_SHUFFLE --n_points $N_POINTS --model $(printf %06d $TEST_ITER).pt --model_path $LOGDIR --set $SET --visualize_results 0 --gender $GENDER --correformer $CORREFORMER
