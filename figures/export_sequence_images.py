gixed bug in variance based patchlets

figures/export_sequence_images.py
@@ -43,10 +43,11 @@ dataset_name = 'dfaust'
 outdir = os.path.join('./log/sequence_images/', dataset_name)
 os.makedirs(outdir, exist_ok=True)
 view = 'front'
-show_patchlets, show_full_pc, reduce_opacity = False, True, False
+show_patchlets, show_full_pc, reduce_opacity = True, True, False
 # n_sequences = 1
 sequence_id = [14]
-patchlet_ids = [2, 50, 100]
+# patchlet_ids = [2, 50, 100]
+patchlet_ids = [0, 10, 100]
 frames_per_clip = 64
 
 
@@ -61,7 +62,7 @@ else:
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
 
 extract_pachlets = PatchletsExtractor(k=24, npoints=512, sample_mode='nn',
-                                      add_centroid_jitter=0.0)
+                                      add_centroid_jitter=0.0, downsample_method='var')
 
 
 for batch_ind, data in enumerate(dataloader):

models/patchlets.py
@@ -108,27 +108,24 @@ class PatchletsExtractor(nn.Module):
             if self.downsample_method == 'fps':
                 #select a subset of the points using fps for maximum coverage
                 selected_idxs = utils.farthest_point_sample(point_seq[:, 0].contiguous(), self.npoints).to(torch.int64)
-                patchlet_points = utils.index_points(patchlet_points,
-                                                     selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1,
-                                                                                                          self.npoints))
-                patchlet_feats = utils.index_points(patchlet_feats,
-                                                    selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1,
-                                                                                                         self.npoints))
-                distances = utils.index_points(distances,
-                                               selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1, self.npoints))
-                idxs = utils.index_points(idxs, selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1, self.npoints))
-                patchlets = utils.index_points(patchlets,
-                                               selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1, self.npoints))
+                selected_idxs = selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1, self.npoints)
+                patchlet_points = utils.index_points(patchlet_points, selected_idxs)
+                patchlet_feats = utils.index_points(patchlet_feats, selected_idxs)
+                distances = utils.index_points(distances, selected_idxs)
+                idxs = utils.index_points(idxs, selected_idxs)
+                patchlets = utils.index_points(patchlets, selected_idxs)
                 n = self.npoints
             else:
                 # select a subset of the points with the largest point variance for maximum temporal movement
-                patchlet_variance = torch.linalg.norm(torch.var(patchlet_points, -2), dim=2)
+                temporal_patchlet_points = patchlet_points.reshape(b, t, n, self.k, d).permute(0, 2, 1, 3, 4).reshape(b,n,-1,d)
+                patchlet_variance = torch.linalg.norm(torch.var(temporal_patchlet_points, -2), dim=-1)
                 _, selected_idxs = torch.topk(patchlet_variance, self.npoints)
-                patchlet_points = utils.index_points(patchlet_points, selected_idxs).reshape(-1, self.npoints)
-                patchlet_feats = utils.index_points(patchlet_feats, selected_idxs.reshape(-1, self.npoints))
-                distances = utils.index_points(distances, selected_idxs.reshape(-1, self.npoints))
-                idxs = utils.index_points(idxs, selected_idxs).reshape(-1, self.npoints)
-                patchlets = utils.index_points(patchlets, selected_idxs).reshape(-1, self.npoints)
+                selected_idxs = selected_idxs.unsqueeze(1).repeat([1, t, 1]).reshape(-1, self.npoints)
+                patchlet_points = utils.index_points(patchlet_points, selected_idxs)#.reshape(-1, self.npoints)
+                patchlet_feats = utils.index_points(patchlet_feats, selected_idxs)#.reshape(-1, self.npoints))
+                distances = utils.index_points(distances, selected_idxs)#.reshape(-1, self.npoints)
+                idxs = utils.index_points(idxs, selected_idxs)#.reshape(-1, self.npoints)
+                patchlets = utils.index_points(patchlets, selected_idxs)#.reshape(-1, self.npoints)
                 n = self.npoints
 
 