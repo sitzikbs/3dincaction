bd8027eba28530775347d9e2aae49d63140c10a0
Itzik ben shabat <sitzikbs@gmail.com>
2023-02-07 22:12:39 +0200

    end of day commit

--- ikeaego/CreateSmallDataset.py
+++ ikeaego/CreateSmallDataset.py
@@ -48,27 +48,28 @@ def SampleAndSave(src_file_path, target_file_path, use_fps, num_points):
     print("Converting {}".format(src_file_path))
     start = time.process_time()
     target_dir = os.path.dirname(os.path.abspath(target_file_path))
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir, exist_ok=True)
+    if not os.path.exists(target_file_path):
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir, exist_ok=True)
 
-    plydata = plyfile.PlyData.read(src_file_path)
-    d = np.asarray(plydata['vertex'].data)
-    pc = np.column_stack([d[p.name] for p in plydata['vertex'].properties])
-    if use_fps:
-        sampled_points = fps_ne(pc, num_points)
-    else:
-        idxs = np.random.shuffle(np.arange(len(pc)))
-        sampled_points = pc[idxs[:len(pc)]]
+        plydata = plyfile.PlyData.read(src_file_path)
+        d = np.asarray(plydata['vertex'].data)
+        pc = np.column_stack([d[p.name] for p in plydata['vertex'].properties])
+        if use_fps:
+            sampled_points = fps_ne(pc, num_points)
+        else:
+            idxs = np.random.shuffle(np.arange(len(pc)))
+            sampled_points = pc[idxs[:len(pc)]]
 
-    pts = list(zip(sampled_points[:, 0], sampled_points[:, 1], sampled_points[:, 2], sampled_points[:, 3],
-                   sampled_points[:, 4], sampled_points[:, 5], sampled_points[:, 6], sampled_points[:, 7],
-                   sampled_points[:, 8]))
-    vertex = np.array(pts, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
-                                  ('red', 'B'), ('green', 'B'), ('blue', 'B')])
-    el = plyfile.PlyElement.describe(vertex, 'vertex')
-    plyfile.PlyData([el], text="").write(target_file_path)
-    end = time.process_time()
-    print("Done. conversion took {}".format(end - start))
+        pts = list(zip(sampled_points[:, 0], sampled_points[:, 1], sampled_points[:, 2], sampled_points[:, 3],
+                       sampled_points[:, 4], sampled_points[:, 5], sampled_points[:, 6], sampled_points[:, 7],
+                       sampled_points[:, 8]))
+        vertex = np.array(pts, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
+                                      ('red', 'B'), ('green', 'B'), ('blue', 'B')])
+        el = plyfile.PlyElement.describe(vertex, 'vertex')
+        plyfile.PlyData([el], text="").write(target_file_path)
+        end = time.process_time()
+        print("Done. conversion took {}".format(end - start))
 
 
 def createSmallDataset(src_dataset, target_dataset, num_points, use_fps, parallelize=False, workers=32):
