81600a745ba11c75a3f1e8646700d561f4720ec2
Itzik ben shabat <sitzikbs@gmail.com>
2023-02-09 11:46:40 +0200

    working on ablation study

--- util_scripts/analyse_point_drop_wandbsweep.py
+++ util_scripts/analyse_point_drop_wandbsweep.py
@@ -1,47 +1,57 @@
 import torch
 import visualization
 import numpy as np
+import sys
+sys.path.append('../dfaust/')
 from DfaustDataset import DfaustActionClipsDataset
-from ikeaaction.IKEAActionDataset import IKEAActionVideoClipDataset
+from ikeaaction.IKEAActionDataset import IKEAActionDatasetClips
 import os
 # import pandas as pd
 import wandb
-
 from models.patchlets import PatchletsExtractor
+import matplotlib.pyplot as plt
 
+sweep = False
 dataset_name = 'ikea'
 output_dir = './patchlet_parameter_ablations/'
 n_examples = 1000
-
 npoints = 512
-k_list = [8, 16, 32, 64]
-# k_list = [16]
-# k = 16
-# sample_mode = 'nn'
-sample_mode_list = ['nn', 'randn']
-# sample_mode_list = ['nn']
-dfaust_augmentation = ['']
-# add_centroid_jitter = 0.005
-centroid_noise_list = [0.0, 0.001, 0.0025, 0.005, 0.0075, 0.01]
-# centroid_noise_list = [0.0]
+frames_per_clip = 64
+
 # Define sweep config
-sweep_configuration = {
-    'method': 'random',
-    'name': 'sweep',
-    'metric': {'goal': 'minimize', 'name': 'final_degradation_rate'},
-    'parameters':
-    {
-        'k': {'values': k_list},
-        'sample_mode': {'values': sample_mode_list},
-        'centroid_noise': {'values': centroid_noise_list}
 
-     }
-}
+if sweep:
+    k_list = [8, 16, 32, 64]
+    sample_mode_list = ['nn']
+    dfaust_augmentation = ['']
+    centroid_noise_list = [0.1]
+    # k_list = [8, 16, 32, 64]
+    # sample_mode_list = ['nn', 'randn']
 
-sweep_id = wandb.sweep(sweep=sweep_configuration, project='point_drop_sweep')
+    # centroid_noise_list = [0.0, 0.001, 0.0025, 0.005, 0.0075, 0.01]
+    configuration = {
+        'method': 'random',
+        'name': 'sweep',
+        'metric': {'goal': 'minimize', 'name': 'final_degradation_rate'},
+        'parameters':
+        {
+            'k': {'values': k_list},
+            'sample_mode': {'values': sample_mode_list},
+            'centroid_noise': {'values': centroid_noise_list}
+         }
+    }
+    sweep_id = wandb.sweep(sweep=configuration, project='point_drop_sweep')
+else:
+    dfaust_augmentation = ['']
+    configuration = {
+            'k':  16,
+            'sample_mode': 'nn',
+            'centroid_noise':  0.001
+    }
+
+
 
 def main():
-    run = wandb.init()
 
     k = wandb.config.k
     sample_mode = wandb.config.sample_mode
@@ -52,14 +62,15 @@ def main():
 
     if dataset_name == 'ikea':
         dataset_path = '/home/sitzikbs/Datasets/ANU_ikea_dataset_smaller/'
-        dataset = IKEAActionVideoClipDataset(dataset_path, frames_per_clip=64, set='train', n_points=4096, input_type='pc', camera='dev3',
-                          mode='img', cache_capacity=1)
+        dataset = IKEAActionDatasetClips(dataset_path,  set='train')
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, num_workers=8,
+                                                       pin_memory=True, shuffle=False, drop_last=True)
     else:
         dataset_path = '/home/sitzikbs/Datasets/dfaust/'
-        dataset = DfaustActionClipsDataset(dataset_path, frames_per_clip=64, set='train', n_points=1024,
+        dataset = DfaustActionClipsDataset(dataset_path, frames_per_clip=frames_per_clip, set='train', n_points=1024,
                                 shuffle_points='fps_each_frame', gender='female', data_augmentation=dfaust_augmentation )
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, num_workers=8,
-                                                   pin_memory=True, shuffle=False, drop_last=True)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, num_workers=0,
+                                                       pin_memory=True, shuffle=False, drop_last=True)
 
     degredation_rate = []
     total_degredation_percentage = []
@@ -92,8 +103,9 @@ def main():
 
             degredation_rate.append(np.mean(point_diff))
             total_degredation_percentage.append(100*(1 - u_points[-1] / n))
-            patchlet_variance.append(torch.var(patchlet_points[batch_ind].permute(1, 0, 2, 3).reshape(npoints, -1, 3),
-                                               dim=1).mean(0).norm().cpu().numpy())
+            # patchlet_variance.append(torch.var(patchlet_points[batch_ind].permute(1, 0, 2, 3).reshape(npoints, -1, 3),
+            #                                    dim=1).mean(0).norm().cpu().numpy())
+            patchlet_variance.append(torch.var(patchlet_points[batch_ind].mean(-2), dim=0).norm(dim=-1).cpu().numpy())
         print('running average Degredation rate: {} points per frame'.format(np.mean(degredation_rate)))
         print('running average total degredation percentage (last - first): {}'.format(
             round(np.mean(total_degredation_percentage), 2)))
@@ -109,11 +121,53 @@ def main():
     print('Average total degredation percentage (last - first): {}'.format(final_degradation_rate))
     print('Average total variance: {}'.format(final_average_patchlet_variance))
 
-    wandb.log({
-        'degredation_rate': final_degradation_rate,
-        'patchlet_variance': final_average_patchlet_variance,
-    })
+    # plot histogram
+    patchlet_variance_flattenned = np.vstack(patchlet_variance).reshape(-1)
+    patchlet_variance_scores = [s for s in patchlet_variance_flattenned]
+    fig = plt.figure()
+    plt.hist(patchlet_variance_scores, bins=1000, range=[0, np.percentile(patchlet_variance_scores, 90)])
+    fig.suptitle('temporal mean center variance', fontsize=20)
+    plt.xlabel('variance', fontsize=18)
+    plt.ylabel('patchlet count', fontsize=16)
+    fig.canvas.draw()
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    images = wandb.Image(data, caption="Variance histogram")
+    wandb.log({"variance histogram": images})
+
+    # plot histogram
+    patchlet_mean_variance = np.mean(np.array(patchlet_variance), 1)
+    fig = plt.figure()
+    plt.hist(patchlet_mean_variance, bins=30, range=[0, np.percentile(patchlet_mean_variance, 90)])
+    fig.suptitle('temporal mean center mean variance', fontsize=20)
+    plt.xlabel('mean variance', fontsize=18)
+    plt.ylabel('patchlet count', fontsize=16)
+    fig.canvas.draw()
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    images = wandb.Image(data, caption="Variance histogram")
+    wandb.log({"Mean variance histogram": images})
+
+    # plt.show()
+    # patchlet_variance_scores = [[s] for s in patchlet_variance]
+    # var_table = wandb.Table(data=patchlet_variance_scores, columns=["patchlet variance"])
+    # wandb.log({'var_histogram': wandb.plot.histogram(var_table, "patchlet variance",
+    #                                                  title="Patchlet variance distribution" )})
+
+
+    columns = ["dataset name", "k", "sample_mode", "centroid_jitter", "degredation rate", "patchlet variance"]
+    results_table = wandb.Table(columns=columns, data=[[dataset_name, k, sample_mode, add_centroid_jitter,
+                                                        final_degradation_rate, final_average_patchlet_variance]])
+    wandb.log({"Results summary": results_table})
     run.finish()
 
 # Start sweep job.
-wandb.agent(sweep_id, function=main, count=4)
+
+
+if __name__ == "__main__":
+    if sweep:
+        wandb.agent(sweep_id, function=main, count=4)
+    else:
+        run = wandb.init()
+        wandb.config.update(configuration)  # adds all of the arguments as config variables
+        main()
