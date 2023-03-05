c110f0d12029c01516592c066c65e9c1c1313963
Itzik ben shabat <sitzikbs@gmail.com>
2023-03-05 10:32:06 +0200

    updated model init to have a callable class

--- models/__init__.py
+++ models/__init__.py
@@ -54,15 +54,31 @@ file_name_dict = {
     'p4transformer': 'P4Transformer.py',
 }
 
-def build_model_from_logdir(logdir, model_cfg, num_classes, frames_per_clip):
-    pc_model = model_cfg.get('pc_model')
-    model_instance = __all__[pc_model]
-    model_name = model_instance.__name__
-    file_name = file_name_dict.get(pc_model)
+# def build_model_from_logdir(logdir, model_cfg, num_classes, frames_per_clip):
+#     pc_model = model_cfg.get('pc_model')
+#     model_instance = __all__[pc_model]
+#     model_name = model_instance.__name__
+#     file_name = file_name_dict.get(pc_model)
+#
+#     spec = importlib.util.spec_from_file_location(model_name, os.path.join(logdir, 'models', file_name))
+#     import_model = importlib.util.module_from_spec(spec)
+#     sys.modules[model_name] = import_model
+#     spec.loader.exec_module(import_model)
+#     model = model_instance(model_cfg=model_cfg, num_class=num_classes, n_frames=frames_per_clip)
+#     return model
 
-    spec = importlib.util.spec_from_file_location(model_name, os.path.join(logdir, 'models', file_name))
-    import_model = importlib.util.module_from_spec(spec)
-    sys.modules[model_name] = import_model
-    spec.loader.exec_module(import_model)
-    model = model_instance(model_cfg=model_cfg, num_class=num_classes, n_frames=frames_per_clip)
-    return model
+
+class build_model_from_logdir(object):
+    def __init__(self, logdir, model_cfg, num_classes, frames_per_clip):
+        pc_model = model_cfg.get('pc_model')
+        model_instance = __all__[pc_model]
+        model_name = model_instance.__name__
+        file_name = file_name_dict.get(pc_model)
+
+        spec = importlib.util.spec_from_file_location(model_name, os.path.join(logdir, 'models', file_name))
+        import_model = importlib.util.module_from_spec(spec)
+        sys.modules[model_name] = import_model
+        spec.loader.exec_module(import_model)
+        self.model = model_instance(model_cfg=model_cfg, num_class=num_classes, n_frames=frames_per_clip)
+    def get(self):
+        return self.model
